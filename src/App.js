fa21a242ffc833c34035aa7a24b5872de105426f
tarungajamoni <saitarun.goud1998@gmail.com>
2024-06-08 14:59:06 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Body from "./components/Body";
-import Header from "./components/Header";
 
 function App() {
   return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { validate } from "../utils/validate";
 
 const SignIn = () => {
   const [signUp, setSignUp] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const SignUpForm = () => {
     setSignUp(!signUp);
+  };
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    setErrorMessage(validate(email.current.value, password.current.value));
+    if (!name.current.value) setErrorMessage("Name is not valid");
   };
   return (
     <div className="">
       <Header />
+      {/* background image */}
       <div>
         <img
           className="fixed"
@@ -16,28 +28,37 @@ const SignIn = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/dd4dfce3-1a39-4b1a-8e19-b7242da17e68/86742114-c001-4800-a127-c9c89ca7bbe4/IN-en-20240527-popsignuptwoweeks-perspective_alpha_website_large.jpg"
         />
       </div>
-      <form className="absolute text-white bg-black w-3/12 mx-auto my-36 right-0 p-8 left-0 bg-opacity-80 rounded-lg">
+      {/* login/logout form */}
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute text-white bg-black w-3/12 mx-auto my-36 right-0 p-8 left-0 bg-opacity-80 rounded-lg">
         <h1 className="font-bold text-3xl py-4">
           {signUp ? "Sign Up" : "Sign In"}
         </h1>
         {signUp && (
           <input
+            ref={name}
             type="text"
             placeholder="Name"
             className="w-full p-4 my-4 bg-gray-700 rounded-sm"
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email"
           className="w-full p-4 my-4 bg-gray-700 rounded-sm"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="w-full p-4 my-4 bg-gray-700 rounded-sm"
         />
-        <button className=" bg-red-700 hover:bg-red-800 px-10 p-4 w-full my-6 rounded-lg">
+        <p className="text-red-500 font-semibold text-lg">{errorMessage}</p>
+        <button
+          className=" bg-red-700 hover:bg-red-800 px-10 p-4 w-full my-6 rounded-lg"
+          onClick={handleButtonClick}>
           {signUp ? "SIGN UP" : "SIGN IN"}
         </button>
         <p onClick={SignUpForm}>
